3dc5d24bf8d12f2a7d46fd96e94c12c712091901
Jan Toth <jant@boxdice.com.au>
2015-03-04 09:25:19 +0100

    fix days_in_month for february

--- attendance_generator.rb
+++ attendance_generator.rb
@@ -11,7 +11,7 @@ month = Time.now.month
 #inspired by rails implementation
 COMMON_YEAR_DAYS_IN_MONTH = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-def days_in_month(month)
+def days_in_month(year, month)
   return 29 if month == 2 && Date.gregorian_leap?(year)
   COMMON_YEAR_DAYS_IN_MONTH[month]
 end
@@ -71,7 +71,7 @@ end
 
 opt_parser.parse!
 
-days = days_in_month(month)
+days = days_in_month(year, month)
 
 CSV.open(output_file, "w:utf-8") do |csv|
   for i in 1..days do
